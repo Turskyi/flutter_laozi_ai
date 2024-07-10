900c36d7c0f8746f3349d359b9a39287e045520e
Dmytro Turskyi <dmytro@turskyi.com>
2024-07-10 15:33:24 -0400

    Add support for sending the feedback via email client and removed workaround email endpoint. (#2)
    * :recycle: chore: bump version to 0.0.3+3 and update Android versionCode and versionName to 3
    
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>
    
    * :recycle: Remove email endpoint. Refactor chat_bloc and retrofit_client
    
    Refactor chat_bloc.dart and retrofit_client.dart by removing unnecessary imports and code comments. Clean up the code for better readability and maintainability.
    
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>
    
    ---------
    
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

--- lib/domain_services/email_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_email_sender/flutter_email_sender.dart';
-
-abstract interface class EmailRepository {
-  const EmailRepository();
-
-  Future<void> sendFeedback(Email email);
-}
