560dcf0a85404aaa06245c4ce2a1d193b36705c8
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2025-10-23 18:18:54 -0400

    :recycle: refactor(email_repository): Improve feedback email sending logic with fallbacks
    This commit refactors the email sending functionality to enhance its reliability across different platforms.
    
    - On Web and macOS, it now directly attempts to open the default email client.
    - On other platforms (Android/iOS), it first tries to send the email via the Resend service.
    - If the Resend service fails on Android or iOS, it falls back to using `FlutterEmailSender`.
    - As a final fallback for `FlutterEmailSender` failures, it attempts to launch the default mail client using `url_launcher`.
    - A private helper method, `_launchEmailClient`, has been introduced to encapsulate the logic for launching the email client, reducing code duplication.
    
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

--- lib/application_services/repositories/email_repository_impl.dart
+++ lib/application_services/repositories/email_repository_impl.dart
@@ -43,55 +43,65 @@ class EmailRepositoryImpl {
     );
 
     try {
-      await _resend.sendEmail(
-        from: 'Do Not Reply ${constants.appName} '
-            '<no-reply@${constants.resendEmailDomain}>',
-        to: <String>[constants.supportEmail],
-        subject: supportEmail.subject,
-        text: supportEmail.body,
-      );
+      if (kIsWeb || Platform.isMacOS) {
+        await _launchEmailClient(supportEmail);
+      } else {
+        await _resend.sendEmail(
+          from: 'Do Not Reply ${constants.appName} '
+              '<no-reply@${constants.resendEmailDomain}>',
+          to: <String>[constants.supportEmail],
+          subject: supportEmail.subject,
+          text: supportEmail.body,
+        );
 
-      await _resend.sendEmail(
-        from: 'Do Not Reply ${constants.appName} '
-            '<no-reply@${constants.resendEmailDomain}>',
-        to: customerEmail.recipients,
-        subject: customerEmail.subject,
-        text: customerEmail.body,
-      );
+        await _resend.sendEmail(
+          from: 'Do Not Reply ${constants.appName} '
+              '<no-reply@${constants.resendEmailDomain}>',
+          to: customerEmail.recipients,
+          subject: customerEmail.subject,
+          text: customerEmail.body,
+        );
+      }
     } catch (e) {
       debugPrint('Error sending email: $e');
-      if (kIsWeb || Platform.isMacOS) {
-        final PackageInfo packageInfo = await PackageInfo.fromPlatform();
-        final Uri emailLaunchUri = Uri(
-          scheme: constants.mailToScheme,
-          path: constants.supportEmail,
-          queryParameters: <String, Object?>{
-            constants.subjectParameter:
-                '${translate('feedback.app_feedback')}: '
-                    '${packageInfo.appName}',
-            constants.bodyParameter: customerEmail.body,
-          },
-        );
+      if (!kIsWeb && (Platform.isAndroid || Platform.isIOS)) {
         try {
-          if (await canLaunchUrl(emailLaunchUri)) {
-            await launchUrl(emailLaunchUri);
-            debugPrint(
-              'Feedback email launched successfully via url_launcher.',
-            );
-          } else {
-            throw const EmailLaunchException('error.launch_email_failed');
-          }
-        } catch (urlLauncherError, urlLauncherStackTrace) {
-          final String urlLauncherErrorMessage =
-              'Error launching email via url_launcher: $urlLauncherError';
-          debugPrint(
-            '$urlLauncherErrorMessage\nStackTrace: $urlLauncherStackTrace',
-          );
-          // TODO: throw exception
+          await FlutterEmailSender.send(supportEmail);
+        } catch (e) {
+          debugPrint('Error sending email via FlutterEmailSender: $e');
+          await _launchEmailClient(supportEmail);
         }
-      } else {
-        await FlutterEmailSender.send(supportEmail);
       }
+    }
+  }
+
+  Future<void> _launchEmailClient(Email supportEmail) async {
+    final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    final Uri emailLaunchUri = Uri(
+      scheme: constants.mailToScheme,
+      path: constants.supportEmail,
+      queryParameters: <String, Object?>{
+        constants.subjectParameter: '${translate('feedback.app_feedback')}: '
+            '${packageInfo.appName}',
+        constants.bodyParameter: supportEmail.body,
+      },
+    );
+    try {
+      if (await canLaunchUrl(emailLaunchUri)) {
+        await launchUrl(emailLaunchUri);
+        debugPrint(
+          'Feedback email launched successfully via url_launcher.',
+        );
+      } else {
+        throw const EmailLaunchException('error.launch_email_failed');
+      }
+    } catch (urlLauncherError, urlLauncherStackTrace) {
+      final String urlLauncherErrorMessage =
+          'Error launching email via url_launcher: $urlLauncherError';
+      debugPrint(
+        '$urlLauncherErrorMessage\nStackTrace: $urlLauncherStackTrace',
+      );
+      // TODO: throw exception?
     }
   }
 }
